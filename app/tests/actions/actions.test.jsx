0f9334fc8e64c75b16fbbacf6c40930ee5b7fbb6
maimous@gmail.com <stefaleon@gmail.com>
2017-02-02 02:13:14 +0200

    add actions and action tests

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,45 @@
+const expect = require('expect');
+const actions = require('actions');
+
+describe('Actions', () => {
+    it('should generate text search action', () => {
+        var action = {
+            type: 'SET_SEARCH_TEXT',
+            searchText: 'Some text'
+        };
+        var res = actions.setSearchText(action.searchText);
+
+        expect(res).toEqual(action);
+    });
+
+
+    it('should generate add todo action', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Some text'
+            };
+            var res = actions.addTodo(action.text);
+
+            expect(res).toEqual(action);
+    });
+
+    it('should generate toggleShowCompleted action', () => {
+            var action = {
+                type: 'TOGGLE_SHOW_COMPLETED'
+            };
+            var res = actions.toggleShowCompleted();
+
+            expect(res).toEqual(action);
+    });
+
+    it('should generate toggleTodo action', () => {
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: '1234'
+            };
+            var res = actions.toggleTodo(action.id);
+
+            expect(res).toEqual(action);
+    });
+
+});
